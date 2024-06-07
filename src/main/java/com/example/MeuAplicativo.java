909e32c3b12e361db19427bb7693172c0a24b52e
Elias Barbosa de Sousa <elias.barbosa@unigranrio.br>
2024-06-07 20:04:58 -0300

    first commit

---
+++ src/main/java/com/example/MeuAplicativo.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuAplicativo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuAplicativo.class, args);
+    }
+}
---
+++ src/main/java/com/example/controller/LivroController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import com.example.model.Livro;
+import com.example.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+
+    @Autowired
+    private LivroService livroService;
+
+    @GetMapping
+    public List<Livro> listarLivros() {
+        return livroService.listarTodos();
+    }
+
+    // Outros endpoints aqui
+}
